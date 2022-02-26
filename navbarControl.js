70a65f2bb0a7f6ae1eaf3196925bb9d24fd27194
Farai Tanekha <30661410+codemusltd@users.noreply.github.com>
2022-02-26 23:08:33 +0000

    use media queries to position embedded googlemap as bootstrap is not working'

--- navbarControl.js
+++ navbarControl.js
@@ -5,8 +5,14 @@ logo = document.querySelector('#company_logo')
 
 for(let link of links) link.addEventListener(
     'click', ()=> {
-        //hide other pages
+        //initially, hide all pages
         for (let page of pages) page.style.display = 'none'
+        //hide copyright info on registration page
+        if(link.id === 'registration') {
+            document.querySelector('#copyright_info').style.visibility = 'hidden'
+        }else{
+            document.querySelector('#copyright_info').style.visibility = 'visible'
+        }
         //display only corresponding link
         document.querySelector(`#${link.id}_page`).style.display = 'block' 
     }
