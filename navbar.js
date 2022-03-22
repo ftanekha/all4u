305e7375e7bc446e2cff3a3db895ffc67d79c96e
Farai Tanekha <30661410+codemusltd@users.noreply.github.com>
2022-03-22 12:46:50 +0000

    candidate registration form  -remove redundant contact info fields  -add completed candidate form upload feature  -tidy up

--- navbar.js
+++ navbar.js
@@ -4,12 +4,12 @@ const
 source = document.querySelector('#navbar_template').innerHTML,
 template = Handlebars.compile(source),
 
-navItems = ['home', 'about', 'registration', 'jobs', 'contact'],
+navItems = ['home', 'about', 'jobs', 'registration', 'contact'],
 context = {
     nav: []
 }
-// for(let item of navItems) context.nav.push({id: item, href: `#${item}`})
+for(let item of navItems) context.nav.push({id: item, href: `#${item}`})
 //hide registration till database sorted
-for(let item of navItems.filter(page => page !== 'registration')) context.nav.push({id: item, href: `#${item}`})
+// for(let item of navItems.filter(page => page !== 'registration')) context.nav.push({id: item, href: `#${item}`})
 
 document.querySelector('#navbar').innerHTML = template(context)
