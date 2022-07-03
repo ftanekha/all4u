108f7f2f8f8205766180257daa363d75e43c881d
Farai Tanekha <30661410+codemusltd@users.noreply.github.com>
2022-07-03 23:14:25 +0100

    minor refactoring

--- control/navbar.js
+++ control/navbar.js
@@ -1,14 +1,15 @@
 'use strict'
+document.addEventListener('DOMContentLoaded', ()=>{
+    const 
+    source = document.querySelector('#navbar_template').innerHTML,
+    template = Handlebars.compile(source),
 
-const 
-source = document.querySelector('#navbar_template').innerHTML,
-template = Handlebars.compile(source),
+    navItems = ['home', 'about', 'jobs', 'registration', 'contact'],
+    context = {
+        nav: []
+    }
+    
+    for(let item of navItems) context.nav.push({id: item, href: `#${item}`})
 
-navItems = ['home', 'about', 'jobs', 'registration', 'contact'],
-context = {
-    nav: []
-}
-for(let item of navItems) context.nav.push({id: item, href: `#${item}`})
-//hide registration till database sorted
-// for(let item of navItems.filter(page => page !== 'registration')) context.nav.push({id: item, href: `#${item}`})
-document.querySelector('#navbar').innerHTML = template(context)
+    document.querySelector('#navbar').innerHTML = template(context)
+})
--- control/toggleSocialMediaIconsView.js
+++ control/toggleSocialMediaIconsView.js
@@ -1,17 +1,18 @@
 'use strict'
 
-const 
-calculatedSocialMediaIconsBoundingClientRect = 880, //from document.querySelectorAll('.social-icon').forEach(el => console.log(el.getBoundingClientRect().top))
-mostBottomElements = document.querySelectorAll('.most_bottom_els'),
-socialMediaIcons =  document.querySelector('#social_media_icons')
+document.addEventListener('DOMContentLoaded', ()=>{
+    const 
+    calculatedSocialMediaIconsBoundingClientRect = 880,
+    socialMediaIcons =  document.querySelector('#social_media_icons')
 
-document.addEventListener(
-    'scroll',
-    ()=> {
-        if(document.body.getBoundingClientRect().bottom > calculatedSocialMediaIconsBoundingClientRect){
-            socialMediaIcons.style.display = 'none'
-        }else{
-            socialMediaIcons.style.display = 'block'
+    document.addEventListener(
+        'scroll',
+        ()=> {
+            if(document.body.getBoundingClientRect().bottom > calculatedSocialMediaIconsBoundingClientRect){
+                socialMediaIcons.style.display = 'none'
+            }else{
+                socialMediaIcons.style.display = 'block'
+            }
         }
-    }
-)
+    )
+})
